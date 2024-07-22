8125bd62de98295d18d07454eae4881e76f661fa
Jay Kang <promotion.kang.jay@gmail.com>
2024-07-21 23:32:11 -0500

    install shadcn and revalidate adter adding items

--- bid_app/src/app/page.tsx
+++ bid_app/src/app/page.tsx
@@ -1,20 +1,26 @@
 import { database } from '@/db/database';
 import { bids as bidsSchema } from '@/db/schema';
+//Every ShadCN component is stored in the components folder;
+import { Button } from '@/components/ui/button'; 
+import { Input } from '@/components/ui/input';
+import { revalidatePath } from 'next/cache';
 
 export default async function HomePage() {
 
   const bids = await database.query.bids.findMany(); //fetch all bids *add async to 
 
   return (
-  <main className="">
+  <main className="container mx-auto py-12">
     <form
       action={async (formData: FormData) => {
         "use server";
         await database.insert(bidsSchema).values({});
+        revalidatePath("/"); // tells nextjs you need to reload/refetch the data = refetch bids const, 
+        // rerun this form component and its jsx
       }}
     >
-      <input name="bid" placeholder="Bid" />
-      <button type="submit">Place Bid</button>
+      <Input name="bid" placeholder="Bid" />
+      <Button type="submit">Place Bid</Button>
     </form>
 
     {bids.map(bid => (
